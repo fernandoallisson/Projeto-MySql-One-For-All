a03673b33f96af2175da1a5d8ba4a14711539be6
fernandoallisson <fernando.alisson@ufpe.br>
2023-09-27 17:41:10 -0300

    Resolução do primeiro exercício

--- challenges/desafio1.sql
+++ challenges/desafio1.sql
@@ -5,52 +5,64 @@ CREATE TABLE SpotifyClone.Planos (
     planos_id INT PRIMARY KEY AUTO_INCREMENT,
     plano VARCHAR(255) NOT NULL,
     valor DECIMAL(10, 2) NOT NULL
-) engine = InnoDB;
+);
 
 CREATE TABLE SpotifyClone.Clientes (
     pessoa_usuaria_id INT PRIMARY KEY AUTO_INCREMENT,
     nome_pessoa_usuaria VARCHAR(255) NOT NULL,
     idade INT,
     planos_id INT,
-    data_assinatura DATE,
     FOREIGN KEY (planos_id) REFERENCES SpotifyClone.Planos(planos_id)
-) engine = InnoDB;
+);
 
 CREATE TABLE SpotifyClone.Artistas (
     Artistas_id INT AUTO_INCREMENT PRIMARY KEY,
     artista VARCHAR(255) NOT NULL
-) engine = InnoDB;
+);
+
+CREATE TABLE SpotifyClone.histórico_planos (
+    id_historico INT AUTO_INCREMENT PRIMARY KEY,
+    pessoa_usuaria_id INT NOT NULL,
+    planos_id INT NOT NULL,
+    `date` DATE NOT NULL,
+	FOREIGN KEY (pessoa_usuaria_id) REFERENCES SpotifyClone.Clientes(pessoa_usuaria_id),
+    FOREIGN KEY (planos_id) REFERENCES SpotifyClone.Planos(planos_id)
+);
 
 CREATE TABLE SpotifyClone.Seguindo_Artistas (
+    id_seguimento INT AUTO_INCREMENT PRIMARY KEY,
     pessoa_usuaria_id INT,
-    artistas INT,
+    Artistas_id INT,
     FOREIGN KEY (pessoa_usuaria_id) REFERENCES SpotifyClone.Clientes(pessoa_usuaria_id),
-    FOREIGN KEY (artistas) REFERENCES SpotifyClone.Artistas(Artistas_id)
-) engine = InnoDB;
+    FOREIGN KEY (Artistas_id) REFERENCES SpotifyClone.Artistas(Artistas_id),
+    UNIQUE KEY (pessoa_usuaria_id, Artistas_id)
+);
 
 CREATE TABLE SpotifyClone.Album (
     album_id INT AUTO_INCREMENT PRIMARY KEY,
     album VARCHAR(255) NOT NULL,
-    Artistas_id INT,
-    ano_lancamento INT,
+    Artistas_id INT NOT NULL,
+    ano_lancamento INT NOT NULL,
     FOREIGN KEY (Artistas_id) REFERENCES SpotifyClone.Artistas(Artistas_id)
-) engine = InnoDB;
+);
 
 CREATE TABLE SpotifyClone.Musica (
     Musicas_id INT AUTO_INCREMENT PRIMARY KEY,
     cancoes VARCHAR(255) NOT NULL,
-    duracao_segundo INT,
-    album_id INT,
+    duracao_segundo INT NOT NULL,
+    album_id INT NOT NULL,
     FOREIGN KEY (album_id) REFERENCES SpotifyClone.Album(album_id)
-) engine = InnoDB;
+);
 
 CREATE TABLE SpotifyClone.Historico (
-    pessoa_usuaria_id INT,
-    Musicas_id INT,
-    data_reproducao DATE,
+    id_historico INT AUTO_INCREMENT PRIMARY KEY,
+    pessoa_usuaria_id INT NOT NULL,
+    Musicas_id INT NOT NULL,
+    data_reproducao DATETIME NOT NULL,
     FOREIGN KEY (pessoa_usuaria_id) REFERENCES SpotifyClone.Clientes(pessoa_usuaria_id),
-    FOREIGN KEY (Musicas_id) REFERENCES SpotifyClone.Musica(Musicas_id)
-) engine = InnoDB;
+    FOREIGN KEY (Musicas_id) REFERENCES SpotifyClone.Musica(Musicas_id),
+    UNIQUE KEY (pessoa_usuaria_id, Musicas_id, data_reproducao)
+);
 
 -- Inserção de dados na tabela Planos
 INSERT INTO SpotifyClone.Planos (plano, valor) VALUES
@@ -60,42 +72,54 @@ INSERT INTO SpotifyClone.Planos (plano, valor) VALUES
 ('pessoal', 6.99);
 
 -- Inserção de dados na tabela Clientes
-INSERT INTO SpotifyClone.Clientes (nome_pessoa_usuaria, idade, planos_id, data_assinatura) VALUES
-('Barbara Liskov', 82, 1, '2019-10-20'),
-('Robert Cecil Martin', 58, 1, '2017-01-06'),
-('Ada Lovelace', 37, 2, '2017-12-30'),
-('Martin Fowler', 46, 2, '2017-01-17'),
-('Sandi Metz', 58, 2, '2018-04-29'),
-('Paulo Freire', 19, 3, '2018-02-14'),
-('Bell Hooks', 26, 3, '2018-01-05'),
-('Christopher Alexander', 85, 4, '2019-06-05'),
-('Judith Butler', 45, 4, '2020-05-13'),
-('Jorge Amado', 58, 4, '2017-02-17');
+INSERT INTO SpotifyClone.Clientes (nome_pessoa_usuaria, idade, planos_id) VALUES
+('Barbara Liskov', 82, 1),
+('Robert Cecil Martin', 58, 1),
+('Ada Lovelace', 37, 2),
+('Martin Fowler', 46, 2),
+('Sandi Metz', 58, 2),
+('Paulo Freire', 19, 3),
+('Bell Hooks', 26, 3),
+('Christopher Alexander', 85, 4),
+('Judith Butler', 45, 4),
+('Jorge Amado', 58, 4);
 
 -- Inserção de dados na tabela Artistas
 INSERT INTO SpotifyClone.Artistas (artista) VALUES
 ('Beyoncé'),
 ('Queen'),
 ('Elis Regina'),
+('Baco Exu do Blues'),
 ('Blind Guardian'),
-('Nina Simone'),
-('Baco Exu do Blues');
+('Nina Simone');
+
+-- Inserção de dados na tabela histórico_planos
+INSERT INTO SpotifyClone.histórico_planos (pessoa_usuaria_id, planos_id, `date`) VALUES
+(1, 1, '2019-10-20'),
+(2, 1, '2017-01-06'),
+(3, 2, '2017-12-30'),
+(4, 2, '2017-01-17'),
+(5, 2, '2018-04-29'),
+(6, 3, '2018-02-14'),
+(7, 3, '2018-01-05'),
+(8, 4, '2019-06-05'),
+(9, 4, '2020-05-13'),
+(10, 4, '2017-02-17');
 
 -- Inserção de dados na tabela Seguindo_Artistas
-INSERT INTO SpotifyClone.Seguindo_Artistas (pessoa_usuaria_id, artistas_id) VALUES
+INSERT INTO SpotifyClone.Seguindo_Artistas (pessoa_usuaria_id, Artistas_id) VALUES
 (1, 1),
 (1, 2),
 (1, 3),
 (2, 1),
 (2, 3),
 (3, 2),
-(4, 6),
-(5, 4),
+(4, 4),
 (5, 5),
-(6, 5),
+(5, 6),
+(6, 6),
 (6, 1),
-(7, 5),
-(8, 7),
+(7, 6),
 (9, 3),
 (10, 2);
 
@@ -113,30 +137,32 @@ INSERT INTO SpotifyClone.Album (album, Artistas_id, ano_lancamento) VALUES
 
 -- Inserção de dados na tabela Musica
 INSERT INTO SpotifyClone.Musica (cancoes, duracao_segundo, album_id) VALUES
-('"BREAK MY SOUL"; "VIRGO\'S GROOVE"; "ALIEN SUPERSTAR"', 279, 1),
-('"Don\'t Stop Me Now"', 203, 2),
-('"Under Pressure"', 152, 3),
-('"Como Nossos Pais"', 105, 4),
-('"O Medo de Amar é o Medo de Ser Livre"', 207, 5),
-('"Samba em Paris"', 267, 6),
-('"The Bard\'s Song"', 244, 7),
-('"Feeling Good"', 100, 8);
+("BREAK MY SOUL", 279, 1),
+("VIRGO'S GROOVE",369, 1),
+("ALIEN SUPERSTAR", 116, 1),
+("Don't Stop Me Now", 203, 2),
+("Under Pressure", 152, 3),
+("Como Nossos Pais", 105, 4),
+("O Medo de Amar é o Medo de Ser Livre", 207, 5),
+("Samba em Paris", 267, 6),
+("The Bard's Song", 244, 7),
+("Feeling Good", 100, 8);
 
 -- Inserção de dados na tabela Historico
 INSERT INTO SpotifyClone.Historico (pessoa_usuaria_id, Musicas_id, data_reproducao) VALUES
-(1, 1, '2022-02-28 10:45:55'),
+(1, 8, '2022-02-28 10:45:55'),
 (1, 2, '2020-05-02 05:30:35'),
-(1, 8, '2020-03-06 11:22:33'),
-(2, 8, '2022-08-05 08:05:17'),
-(2, 5, '2020-01-02 07:40:33'),
-(3, 8, '2020-11-13 16:55:13'),
-(3, 3, '2020-12-05 18:38:30'),
-(4, 6, '2021-08-15 17:10:10'),
-(5, 6, '2022-01-09 01:44:33'),
-(5, 3, '2020-08-06 15:23:43'),
-(6, 8, '2017-01-24 00:31:17'),
+(1, 10, '2020-03-06 11:22:33'),
+(2, 10, '2022-08-05 08:05:17'),
+(2, 7, '2020-01-02 07:40:33'),
+(3, 10, '2020-11-13 16:55:13'),
+(3, 2, '2020-12-05 18:38:30'),
+(4, 8, '2021-08-15 17:10:10'),
+(5, 8, '2022-01-09 01:44:33'),
+(5, 5, '2020-08-06 15:23:43'),
+(6, 7, '2017-01-24 00:31:17'),
 (6, 1, '2017-10-12 12:35:20'),
-(7, 2, '2011-12-15 22:30:49'),
-(8, 2, '2012-03-17 14:56:41'),
-(9, 7, '2022-02-24 21:14:22'),
-(10, 4, '2015-12-13 08:30:22');
+(7, 4, '2011-12-15 22:30:49'),
+(8, 4, '2012-03-17 14:56:41'),
+(9, 9, '2022-02-24 21:14:22'),
+(10, 3, '2015-12-13 08:30:22');
