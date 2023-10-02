2e3f0b75481566e7c10131a2d1a71eb9d88ba755
fernandoallisson <fernando.alisson@ufpe.br>
2023-10-02 15:38:04 -0300

    Resolução do desafio 3

---
+++ challenges/desafio3.sql
@@ -0,0 +1,9 @@
+SELECT
+    C.nome_pessoa_usuaria AS pessoa_usuaria,
+    COUNT(H.Musicas_id) AS musicas_ouvidas,
+    SUM(M.duracao_segundo / 60) AS total_minutos
+FROM SpotifyClone.Clientes C
+LEFT JOIN SpotifyClone.Historico H ON C.pessoa_usuaria_id = H.pessoa_usuaria_id
+LEFT JOIN SpotifyClone.Musica M ON H.Musicas_id = M.Musicas_id
+GROUP BY C.nome_pessoa_usuaria
+ORDER BY C.nome_pessoa_usuaria;
