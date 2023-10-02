a9f0035c2bc70fbf04110c81ea84d1c9e9c6d01b
fernandoallisson <fernando.alisson@ufpe.br>
2023-10-02 15:49:59 -0300

    Resolução do desafio 3 e 4

--- challenges/desafio4.sql
+++ challenges/desafio4.sql
@@ -1,9 +1,10 @@
 SELECT
     C.nome_pessoa_usuaria AS pessoa_usuaria,
-    COUNT(H.Musicas_id) AS musicas_ouvidas,
-    SUM(M.duracao_segundo / 60) AS total_minutos
+    CASE
+        WHEN MAX(H.data_reproducao) >= '2021-01-01' THEN 'Ativa'
+        ELSE 'Inativa'
+    END AS status_pessoa_usuaria
 FROM SpotifyClone.Clientes C
 LEFT JOIN SpotifyClone.Historico H ON C.pessoa_usuaria_id = H.pessoa_usuaria_id
-LEFT JOIN SpotifyClone.Musica M ON H.Musicas_id = M.Musicas_id
 GROUP BY C.nome_pessoa_usuaria
 ORDER BY C.nome_pessoa_usuaria;
